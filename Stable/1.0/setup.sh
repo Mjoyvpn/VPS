3b9990ca47a61fc8fe1eafafc1777d7dcc7de070
Mjoyvpn <112147124+Mjoyvpn@users.noreply.github.com>
2022-09-16 15:21:55 +0800

    Update setup.sh

--- Stable/1.0/setup.sh
+++ Stable/1.0/setup.sh
@@ -6,10 +6,7 @@
 #               quick Setup script from one click installation
 # Created     : 16-05-2022 ( 16 May 2022 )
 # OS Support  : Ubuntu & Debian
-# Auther      : WildyDev21
-# WebSite     : https://wildydev21.com
-# Github      : github.com/wildydev21
-# License     : MIT License
+# Auther      : joysmark
 # ==================================================================
 
 # // Export Color & Information
