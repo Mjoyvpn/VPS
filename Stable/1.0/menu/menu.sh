2780e67c4765b1f017b3229653afa8c8216ee1a0
Mjoyvpn <112147124+Mjoyvpn@users.noreply.github.com>
2022-09-16 09:11:23 +0800

    Update menu.sh

--- Stable/1.0/menu/menu.sh
+++ Stable/1.0/menu/menu.sh
@@ -50,7 +50,7 @@ export AUTHER="WildyDev21";
 export ROOT_DIRECTORY="/etc/wildydev21";
 export CORE_DIRECTORY="/usr/local/wildydev21";
 export SERVICE_DIRECTORY="/etc/systemd/system";
-export SCRIPT_SETUP_URL="https://raw.githubusercontent.com/Mjoyvpn/VPS/main/vpn-script";
+export SCRIPT_SETUP_URL="https://raw.githubusercontent.com/Mjoyvpn/VPS/main";
 export REPO_URL="https://repository.wildydev21.com";
 
 # // Checking Your Running Or Root or no
@@ -77,7 +77,7 @@ else
 fi
 
 # // Exporting Network Information
-wget -qO- --inet4-only 'https://raw.githubusercontent.com/Mjoyvpn/VPS/main/vpn-script/get-ip_sh' | bash;
+wget -qO- --inet4-only 'https://raw.githubusercontent.com/Mjoyvpn/VPS/main/get-ip_sh' | bash;
 source /root/ip-detail.txt;
 export IP_NYA="$IP";
 export ASN_NYA="$ASN";
@@ -88,7 +88,7 @@ export COUNTRY_NYA="$COUNTRY";
 export TIME_NYA="$TIMEZONE";
 
 # // Check Blacklist
-export CHK_BLACKLIST=$( wget -qO- --inet4-only 'https://api.wildydev21.com/vpn-script/blacklist.php?ip='"${IP_NYA}"'' );
+export CHK_BLACKLIST=$( wget -qO- --inet4-only 'https://api.wildydev21.com/blacklist.php?ip='"${IP_NYA}"'' );
 if [[ $( echo $CHK_BLACKLIST | jq -r '.respon_code' ) == "127" ]]; then
     SKIP=true
 else
@@ -112,7 +112,7 @@ if [[ $LCN_KEY == "" ]]; then
     exit 1;
 fi
 
-export API_REQ_NYA=$( wget -qO- --inet4-only 'https://api.wildydev21.com/vpn-script/secret/chk-rnn.php?scrty_key=61716199-7c73-4945-9918-c41133d4c94e&ip_addr='"${IP_NYA}"'&lcn_key='"${LCN_KEY}"'' );
+export API_REQ_NYA=$( wget -qO- --inet4-only 'https://api.wildydev21.com/secret/chk-rnn.php?scrty_key=61716199-7c73-4945-9918-c41133d4c94e&ip_addr='"${IP_NYA}"'&lcn_key='"${LCN_KEY}"'' );
 if [[ $( echo ${API_REQ_NYA} | jq -r '.respon_code' ) == "104" ]]; then
     SKIP=true;
 else
@@ -293,7 +293,7 @@ case $choosemu in
         echo -e "${OKEY} Starting Generating Certificate";
         rm -rf /root/.acme.sh;
         mkdir -p /root/.acme.sh;
-        wget -q -O /root/.acme.sh/acme.sh "https://raw.githubusercontent.com/Mjoyvpn/VPS/main/vpn-script/Resource/Core/acme.sh";
+        wget -q -O /root/.acme.sh/acme.sh "https://raw.githubusercontent.com/Mjoyvpn/VPS/main/Resource/Core/acme.sh";
         chmod +x /root/.acme.sh/acme.sh;
         sudo /root/.acme.sh/acme.sh --register-account -m kibocelcom@gmail.com;
         sudo /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256 -ak ec-256;
@@ -394,7 +394,7 @@ case $choosemu in
         systemctl restart nginx; systemctl restart fail2ban; systemctl restart ssr-server; systemctl restart dropbear; systemctl restart ssh; systemctl restart stunnel4; systemctl restart sslh;
         clear; echo -e "${OKEY} Successfull Restarted All Service";
     ;;
-    28) cd /root/; wget -q -O /root/update.sh "https://raw.githubusercontent.com/Mjoyvpn/VPS/main/vpn-script/Stable/update.sh"; chmod +x /root/update.sh; ./update.sh; rm -f /root/update.sh ;;
+    28) cd /root/; wget -q -O /root/update.sh "https://raw.githubusercontent.com/Mjoyvpn/VPS/main/Stable/update.sh"; chmod +x /root/update.sh; ./update.sh; rm -f /root/update.sh ;;
     29)
             clear
             # // clearlog
